f2e71b11458ab5b68acf232b7d8a05db91561baa
Eugene July <evgeshka120@gmail.com>
2023-12-12 15:03:20 +0300

    lab_8 ~

--- lab_8/lab_8.py
+++ lab_8/lab_8.py
@@ -2,37 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Начальные частоты
-freq_x = 100  # Герц
-freq_y = 300  # Герц
+# Исходные частоты
+freq_x = 100  # Частота по оси X в Герцах
+freq_y = 300  # Частота по оси Y в Герцах
 
-# Небольшое отличие частот
-delta_freq_x = 0.1
-delta_freq_y = 0.2
-
-# Временной интервал и количество точек
-dt = 0.01
-num_points = 1000
-
-# Функция для обновления графика в анимации
-def update(frame):
-    global freq_x, freq_y
-
-    # Обновление частот с небольшим отличием
-    freq_x += delta_freq_x
-    freq_y += delta_freq_y
-
-    t = np.linspace(0, 1, num_points)
+# Создание фигуры Лиссажу
+def lissajous(t, freq_x, freq_y):
     x = np.sin(2 * np.pi * freq_x * t)
     y = np.sin(2 * np.pi * freq_y * t)
+    return x, y
 
-    # Очищаем предыдущий график и рисуем новый
+def update(frame):
     plt.clf()
+    t = np.linspace(0, frame / 10, 1000)  # Используйте больше или меньше 10, чтобы изменить скорость анимации
+    x, y = lissajous(t, freq_x, freq_y)
     plt.plot(x, y)
-    plt.title(f'Lissajous Figure: {freq_x} Hz vs {freq_y} Hz')
+    plt.title(f'Lissajous Figure (Time: {t[-1]:.1f} s)')
+    plt.xlabel('X-axis (units)')
+    plt.ylabel('Y-axis (units)')
 
-# Создаем анимацию
-ani = FuncAnimation(plt.figure(), update, frames=range(100), interval=100)
+# Создание анимации
+fig, ax = plt.subplots()
+ani = FuncAnimation(fig, update, frames=np.arange(0, 1000), interval=50)
 
-# Сохраняем анимацию в файл GIF
 ani.save('lissajous_animation.gif', writer='pillow', fps=15)
